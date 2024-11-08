2a737cd36d7ec7e9b59f6bd3d6344c1f4f3aaef0
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-08 15:52:47 +0530

    basic cards in project section done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,44 +9,46 @@ import { HeroOrbit } from '@/components/HeroOrbit';
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div
-        className="absolute inset-0 -z-30"
-        style={{ backgroundImage: `url(${grainImage.src})`, opacity: 0.05 }}
-      ></div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820px] hero-ring"></div>
-      <div className="size-[1020px] hero-ring"></div>
-      <div className="size-[1220px] hero-ring"></div>
-      <HeroOrbit size={800} rotation={-72}>
-        <StarIcon className="size-28 text-emerald-300" />
-      </HeroOrbit>
-      <HeroOrbit size={550} rotation={20}>
-        <StarIcon className="size-12 text-emerald-300" />
-      </HeroOrbit>
-      <HeroOrbit size={590} rotation={98}>
-        <StarIcon className="size-8 text-emerald-300" />
-      </HeroOrbit>
-      <HeroOrbit size={430} rotation={-14}>
-        <SparkleIcon className="size-8 text-emerald-300/20" />
-      </HeroOrbit>
-      <HeroOrbit size={440} rotation={79}>
-        <SparkleIcon className="size-8 text-emerald-300/20" />
-      </HeroOrbit>
-      <HeroOrbit size={530} rotation={178}>
-        <SparkleIcon className="size-10 text-emerald-300/20" />
-      </HeroOrbit>
-      <HeroOrbit size={710} rotation={144}>
-        <SparkleIcon className="size-14 text-emerald-300/20" />
-      </HeroOrbit>
-      <HeroOrbit size={720} rotation={86}>
-        <div className="size-3 bg-emerald-300/20 rounded-full" />
-      </HeroOrbit>
-      <HeroOrbit size={650} rotation={-5}>
-        <div className="size-3 bg-emerald-300/20 rounded-full" />
-      </HeroOrbit>
-      <HeroOrbit size={520} rotation={-41}>
-        <div className="size-2 bg-emerald-300/20 rounded-full" />
-      </HeroOrbit>
+      <div className="absolute inset-0 mask-gradient">
+        <div
+          className="absolute inset-0 -z-30"
+          style={{ backgroundImage: `url(${grainImage.src})`, opacity: 0.05 }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={86}>
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+      </div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[120px]" alt="person" />
@@ -62,8 +64,11 @@ export const HeroSection = () => {
             I&apos;m Vinay Thakor
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
-            I specialize in building full-stack web applications, mobile apps,
-            and Web3 projects. Let&apos;s discuss your next project.
+            I specialize in building{' '}
+            <span className="text-white/90">Full-Stack</span> web applications,
+            <br />
+            Advance System Design and Web3 projects.
+            <br /> Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,46 +1,106 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import AksharLandingPage from '@/assets/images/akshar-landing-page.png';
+import WanderHostLandingPage from '@/assets/images/wanderhost-landing-page.png';
+import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png';
+import Image from 'next/image';
+import CheckIcon from '@/assets/icons/check-circle.svg';
+import ArrowUpgright from '@/assets/icons/arrow-up-right.svg';
+import { div } from 'framer-motion/client';
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: 'Akshar',
+    year: '2024',
+    title: 'Smart Classroom Management System',
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: 'Designed a user-friendly interface' },
+      { title: 'Implemented a secure login system' },
+      { title: 'System Design and Development' },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: 'https://github.com/Akshar-scms/Akshar-frontend-react',
+    image: AksharLandingPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: 'Wanderhost',
+    year: '2023',
+    title: 'A hosting platform for travelers',
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: 'MERN Stack Development' },
+      { title: 'Created a responsive website' },
+      { title: 'CRUD operations' },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
+    link: 'https://github.com/vinay-th/Wanderhost',
+    image: WanderHostLandingPage,
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
+    company: 'Quantum Dynamics',
+    year: '2023',
+    title: 'AI Startup Landing Page',
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: 'ReactJS Development' },
+      { title: 'UI/UX Design' },
+      { title: 'SEO Optimization' },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
+    link: 'https://youtu.be/Z7I5uSRHMHg',
     image: aiStartupLandingPage,
   },
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            Real World Projects
+          </p>
+        </div>
+
+        <h2 className="font-serif text-3xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4">
+          See how i transformed my ideas into engaging digital experiences.
+        </p>
+        <div className="flex flex-col mt-10 gap-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden
+              after:z-10 after:content-[''] after:absolute after:inset-0 after:border-2 after:border-white/20 after:rounded-3xl px-8 pt-8"
+            >
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                <div>{project.company}</div>
+                <span>&bull;</span>
+                <div>{project.year}</div>
+              </div>
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4" />
+              <ul className="flex flex-col gap-4 mt-4">
+                {project.results.map((result) => (
+                  <li
+                    className="flex gap-2 text-sm text-white/50"
+                    key={result.title}
+                  >
+                    <CheckIcon className="size-5" />
+                    <span>{result.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button className="bg-white text-gray-900 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  <span>View Project</span>
+                  <ArrowUpgright className="size-4" />
+                </button>
+              </a>
+              <Image
+                src={project.image}
+                alt={project.title}
+                className="mt-8 -mb-4"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
