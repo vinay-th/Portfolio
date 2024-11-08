08a253d63373f7cfda56c6aafbbb50bad8a4aad4
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-08 17:10:25 +0530

    Projects section completed

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,6 +4,7 @@ import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png';
 import Image from 'next/image';
 import CheckIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpgright from '@/assets/icons/arrow-up-right.svg';
+import GrainImage from '@/assets/images/grain.jpg';
 import { div } from 'framer-motion/client';
 
 const portfolioProjects = [
@@ -33,21 +34,21 @@ const portfolioProjects = [
   },
   {
     company: 'Quantum Dynamics',
-    year: '2023',
+    year: '2024',
     title: 'AI Startup Landing Page',
     results: [
       { title: 'ReactJS Development' },
       { title: 'UI/UX Design' },
       { title: 'SEO Optimization' },
     ],
-    link: 'https://youtu.be/Z7I5uSRHMHg',
+    link: '#',
     image: aiStartupLandingPage,
   },
 ];
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16">
+    <section className="pb-16 lg:py-24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
@@ -55,48 +56,64 @@ export const ProjectsSection = () => {
           </p>
         </div>
 
-        <h2 className="font-serif text-3xl text-center mt-6">
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
           Featured Projects
         </h2>
-        <p className="text-center text-white/60 mt-4">
+        <p className="text-center md:text-large lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
           See how i transformed my ideas into engaging digital experiences.
         </p>
-        <div className="flex flex-col mt-10 gap-20">
+        <div className="md:mt-20 flex flex-col mt-10 gap-20">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
               className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden
-              after:z-10 after:content-[''] after:absolute after:inset-0 after:border-2 after:border-white/20 after:rounded-3xl px-8 pt-8"
+              after:z-10 after:content-[''] after:absolute after:inset-0 after:border-2 after:border-white/20 after:rounded-3xl px-8 pt-8
+              md:pt-12 md:px-10 after:pointer-events-none lg:pt-16 lg:px-20"
             >
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                <div>{project.company}</div>
-                <span>&bull;</span>
-                <div>{project.year}</div>
+              <div
+                className="absolute inset-0 -z-10"
+                style={{
+                  backgroundImage: `url(${GrainImage.src})`,
+                  opacity: 0.05,
+                }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <div>{project.company}</div>
+                    <span>&bull;</span>
+                    <div>{project.year}</div>
+                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li
+                        className="flex gap-2 text-sm md:text-base text-white/50"
+                        key={result.title}
+                      >
+                        <CheckIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-900 h-12 w-full md:w-auto px-8 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>View Project</span>
+                      <ArrowUpgright className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative ">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
               </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="flex flex-col gap-4 mt-4">
-                {project.results.map((result) => (
-                  <li
-                    className="flex gap-2 text-sm text-white/50"
-                    key={result.title}
-                  >
-                    <CheckIcon className="size-5" />
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-              <a href={project.link}>
-                <button className="bg-white text-gray-900 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                  <span>View Project</span>
-                  <ArrowUpgright className="size-4" />
-                </button>
-              </a>
-              <Image
-                src={project.image}
-                alt={project.title}
-                className="mt-8 -mb-4"
-              />
             </div>
           ))}
         </div>
