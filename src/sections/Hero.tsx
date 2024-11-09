18f3ce44affa8c9bbec23d86679b2610e1ed2f49
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-09 12:11:25 +0530

    ping animation added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -53,7 +53,9 @@ export const HeroSection = () => {
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[120px]" alt="person" />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="ping-online bg-green-500 size-2.5 rounded-full relative ">
+              <div className=" bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div>
+            </div>
             <div className="text-sm font-medium">
               Available for new projects
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,17 @@ const config: Config = {
         sans: 'var(--font-sans)',
         serif: 'var(--font-serif)',
       },
+      animation: {
+        'ping-large': 'ping-large 1s ease-in-out infinite',
+      },
+      keyframes: {
+        'ping-large': {
+          '75%, 100%': {
+            transform: 'scale(3)',
+            opacity: '0',
+          },
+        },
+      },
     },
   },
   plugins: [],
