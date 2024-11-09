0213ee0bd0a0e63c20ede29e75dae0b4de24a76b
Vinay Thakor <vinay.th.24@gmail.com>
2024-11-09 12:02:38 +0530

    fixed links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,4 @@
-import ArrowUpRight from '@/assets/icons/arrow-up-right.svg';
+import ArrowUpright from '@/assets/icons/arrow-up-right.svg';
 
 const links = [
   { title: 'GitHub', href: 'https://github.com/vinay-th' },
@@ -10,21 +10,20 @@ const links = [
 export const Footer = () => {
   return (
     <footer className="relative z-10 overflow-x-clip">
-      <div className="absolute h-[400px] w-full bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">&copy; 2024. All rights reserved.</div>
-          <nav className="flex flex-col md:flex-row items-center gap-8 z-50">
+          <nav className="flex flex-col md:flex-row items-center gap-8">
             {links.map((link) => (
               <a
                 key={link.title}
                 href={link.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-flex items-center gap-1.5 text-white hover:text-emerald-400 transition-colors duration-200"
+                className="inline-flex items-center gap-1.5 text-white hover:text-emerald-300"
+                style={{ position: 'relative', zIndex: 10 }}
               >
                 <span className="font-semibold">{link.title}</span>
-                <ArrowUpRight className="size-4" />
+                <ArrowUpright className="size-4" />
               </a>
             ))}
           </nav>
